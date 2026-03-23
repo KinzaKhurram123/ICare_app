ff7d527ed7c67c4e1b9f5e4f2d34681e9c16f2e7
muhammadadil0 <adilraxiq64@gmail.com>
2026-03-23 20:40:57 +0500

    fix: enable core library desugaring for FCM notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,11 +22,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.icare"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -34,10 +31,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            
         }
     }
 }
